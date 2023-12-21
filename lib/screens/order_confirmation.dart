ae1b4559204a72e05b3f14b12c29b1b0f1f2b74e
Chandra Prasetya <chandraprasetyap08@gmail.com>
2023-12-22 02:19:54 +0700

    feat: create order confirm screen

---
+++ lib/screens/order_confirmation.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OrderConfirm extends StatefulWidget {
+  const OrderConfirm({super.key});
+
+  @override
+  State<OrderConfirm> createState() => _OrderConfirmState();
+}
+
+class _OrderConfirmState extends State<OrderConfirm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
