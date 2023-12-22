bba1a2bc148f5a8e6fd68c3ec9c812f235f0c6f3
waliid ilham <92657818+waliidilham@users.noreply.github.com>
2023-12-22 17:09:13 +0700

    nambahin beberapa hal di profile

---
+++ lib/screens/profile/updateProfileScreen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:sisa/constant/image_string.dart';
+
+class UpdateProfile extends StatefulWidget {
+  const UpdateProfile({super.key});
+
+  @override
+  State<UpdateProfile> createState() => _UpdateProfileState();
+}
+
+class _UpdateProfileState extends State<UpdateProfile> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: () => Get.back(),
+            icon: const Icon(LineAwesomeIcons.angle_left)),
+        title: Text("Edit Profile",
+            style: Theme.of(context).textTheme.headlineMedium),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Stack(
+                children: [
+                  SizedBox(
+                    width: 120,
+                    height: 120,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: Image(image: AssetImage(tProfileImage)),
+                    ),
+                  ),
+                  Positioned(
+                    child: Container(
+                      width: 35,
+                      height: 35,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          color: Colors.amber[100]),
+                      child: const Icon(
+                        LineAwesomeIcons.camera,
+                        color: Colors.black,
+                        size: 20,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 50),
+              Form(
+                  child: Column(
+                children: [],
+              ))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
